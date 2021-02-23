e3e7074d6bb804344f53f39c94ca7e9fb4e852d3
Steve Davis <hmsdavis@gmail.com>
2021-02-22 23:18:11 -0500

    Decode Responses

--- lambda/reply.js
+++ lambda/reply.js
@@ -3,8 +3,13 @@ const Twilio = require('twilio');
 exports.getTwilio = () => new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 exports.createTwilioMessage = (client, body, to, from) => {
-  client.messages.create({
-    body,
+  console.info(`Sending: ${JSON.stringify({
+    body: decodeURI(body),
+    to,
+    from,
+  })}`);
+  return client.messages.create({
+    body: decodeURI(body),
     to,
     from,
   });
@@ -14,7 +19,15 @@ exports.handler = async (event) => {
   const twilioClient = module.exports.getTwilio();
   const to = `+${process.env.TWILIO_SUMMARY_NUMBER}`;
   const from = `+${process.env.TWILIO_NUMBER}`;
-  await module.exports.createTwilioMessage(twilioClient, event.Body, to, from);
 
+  console.info(`Sending message from ${from}, going to ${to}, that says ${event.Body}`);
+
+  try {
+    await module.exports.createTwilioMessage(twilioClient, event.Body, to, from);
+  } catch (error) {
+    console.error('Could not send message', error);
+  }
+
+  console.info('Message Sent');
   return '<?xml version="1.0" encoding="UTF-8"?><Response><Message><Body>I will forward that along to Steve!</Body></Message></Response>';
 };
--- test/reply/reply.test.js
+++ test/reply/reply.test.js
@@ -13,12 +13,12 @@ describe('replies', () => {
     sandbox.stub(reply, 'getTwilio').callsFake(() => ({
       messages: {
         create: (body) => {
-          expect(body).to.deep.equal({ body: 'asd', to: '+123', from: '+456' });
+          expect(body).to.deep.equal({ body: 'This is a test!', to: '+123', from: '+456' });
         },
       },
     }));
 
-    const results = await reply.handler({ Body: 'asd' });
+    const results = await reply.handler({ Body: encodeURI('This is a test!') });
     expect(results).to.equal('<?xml version="1.0" encoding="UTF-8"?><Response><Message><Body>I will forward that along to Steve!</Body></Message></Response>');
   });
 });
